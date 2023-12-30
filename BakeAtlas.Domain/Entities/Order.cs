4d494d63b8baedd99baa2c233f33c8022658228a
Gemma <jemmimah.abdul@decagon.dev>
2023-12-30 23:59:39 +0100

    Domain SetUp

---
+++ BakeAtlas.Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeAtlas.Domain.Entities
+{
+    public class Order
+    {
+        public string CustomerFullName { get; set; }
+        public string Description { get; set; }
+        public List<BakeryProduct> products { get; set; }
+    
+
+    }
+}
