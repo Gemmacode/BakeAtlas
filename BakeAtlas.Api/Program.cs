4ec3c65e75ace6885d649c6770fc0891b66cd3d7
Gemma <jemmimah.abdul@decagon.dev>
2023-12-31 13:21:02 +0100

    Migration

--- BakeAtlas.Api/Program.cs
+++ BakeAtlas.Api/Program.cs
@@ -1,8 +1,21 @@
+using BakeAtlas.Domain.Entities;
+using BakeAtlas.Persistence.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddIdentity<Customer, IdentityRole>()
+            .AddEntityFrameworkStores<BakeAtlasDbContext>()
+            .AddDefaultTokenProviders();
+
+builder.Services.AddDbContext<BakeAtlasDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ BakeAtlas/Context/BakeAtlasDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BakeAtlas.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeAtlas.Persistence.Context
+{
+    public class BakeAtlasDbContext : IdentityDbContext<Customer>
+    {
+        public BakeAtlasDbContext(DbContextOptions<BakeAtlasDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Order> Orders { get; set; } 
+        public DbSet<BakeryProduct> BakeryProducts { get; set;}
+    }
+}
