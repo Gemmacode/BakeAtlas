b0f9de72198505fb9f3311dfc876fbd7e6d2ec05
Gemma <jemmimah.abdul@decagon.dev>
2024-02-19 00:40:13 +0100

    Updated

--- BakeAtlas.Domain/Entities/OrderItem.cs
+++ BakeAtlas.Domain/Entities/OrderItem.cs
@@ -11,16 +11,27 @@ namespace BakeAtlas.Domain.Entities
     {
         
         //[ForeignKey("OrderId")]
+        //public Order Order { get; set; }
+
+        //public string BakeryProductId { get; set; }
+        //public BakeryProduct BakeryProduct { get; set; }
+        //public string CustomerId { get; set; }
+        //public Customer Customer { get; set; }
+
+        //public string Quantity { get; set; }
+
+        [ForeignKey("OrderId")]
+        public string OrderId { get; set; }
+
+        // Navigation property to access the associated order
         public Order Order { get; set; }
 
+        // ID of the bakery product being ordered
         public string BakeryProductId { get; set; }
-        public BakeryProduct BakeryProduct { get; set; }
-        public string CustomerId { get; set; }
-        public Customer Customer { get; set; }
 
-        public string Description { get; set; }
-
-        public string Quantity { get; set; }
-       
+        // Quantity of the bakery product being ordered
+        public int Quantity { get; set; }
     }
+
 }
+
