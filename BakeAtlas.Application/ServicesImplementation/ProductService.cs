4df0527a675d65c3b104fcb55ff543059eccf7d9
Gemma <jemmimah.abdul@decagon.dev>
2024-05-20 10:42:45 +0100

    Updated

---
+++ BakeAtlas.Application/ServicesImplementation/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using BakeAtlas.Domain.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BakeAtlas.Application.ServicesImplementation
+{
+    public class ProductService
+    {
+        private static IList<BakeryProductDTO>productList = new List<BakeryProductDTO>();  
+        private readonly RapidApiService _rapidApiService;
+
+        public ProductService(RapidApiService rapidApiService)
+        {
+            _rapidApiService = rapidApiService;
+        }
+            
+        public async Task<BakeryProductDTO> GetCakeDataAsync(int id)
+        {
+            var result = await _rapidApiService.GetCakeDataAsync(id);
+            var cakeData = JsonConvert.DeserializeObject<BakeryProductDTO>(result);  
+            return cakeData;    
+        }
+
+        public  void AddProduct (BakeryProductDTO productDto) 
+        { 
+            productList.Add(productDto);
+        }
+
+        public IEnumerable <BakeryProductDTO> GetAllProducts()
+        {
+            return productList;
+        }
+
+        public BakeryProductDTO GetProductById(string id)
+        {
+            return productList.FirstOrDefault(product=>product.Equals(id));
+        }
+    }
+}
